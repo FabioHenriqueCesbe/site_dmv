362e6e2aa452fe18b0809eb493e8f4d21f9a188d
Fábio <fabio.silva@cesbe.com.br>
2026-01-14 17:26:27 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -152,6 +152,7 @@ export default function Home() {
                 <Image
                   src="/Gemini_Generated_Image_oorxu4oorxu4oorx.png"
                   alt="Sobre Nós"
+                  fill
                   className="object-cover transition-transform duration-500 hover:scale-105"
                 />
               </div>
